0d2cfe5e498de51dd83bba32808d6de0942ddda2
Kurt F <kurtfloyd18@gmail.com>
2018-03-11 20:55:12 -0400

    Continued implementation

--- src/backend/Clock.java
+++ src/backend/Clock.java
@@ -1,7 +1,11 @@
 package backend;
 
+import java.time.Duration;
+import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.PriorityQueue;
 /**
  * 
@@ -9,81 +13,133 @@ import java.util.PriorityQueue;
  *
  */
 public class Clock {
+	//A queue of all alarms.
+	private LinkedList<Alarm> alarms;
+	
+	Clock(){
+		alarms = new LinkedList<Alarm>();
+	}
+	
+	/**
+	 * Checks the given date and time to see if an alarm time has been reached. Only the soonest alarm is checked.
+	 */
+	public void tick(LocalTime t, LocalDate d){
+		//Change the time based on current weather/traffic conditions
+		Duration traffic = getTrafficShift();
+		Duration weather = getWeatherShift();
+		//If there are no alarms set to go off today, don't bother checking. 
+		if(alarms.peek() != null && alarms.getFirst().getDay().equals(d)){
+		if(traffic != null)
+			t.plus(traffic);
+		if(weather != null)
+			t.plus(weather);
+		//If the shifted time either passed by or is at the soonest alarm time(s), trip it/them.
+		while(!alarms.isEmpty() && alarms.getFirst().getTime().compareTo(t) <= 0){
+			Alarm alarm = alarms.removeFirst();
+			this.tripAlarm(alarm);
+			//If the alarm is set to repeat, re-add it but set for tomorrow. 
+			if(alarm.doesRepeat())
+				this.setAlarm(alarm.getTime(), alarm.getDay().plusDays(1), true);
+			}
+		}
+	}
+	
+	/**
+	 * Set alarm with the default tone
+	 * @param t the time and date the alarm should go off
+	 * @param d the date the alarm is set for
+	 * @param r whether the alarm repeats or not
+	 */
+	public void setAlarm(LocalTime t, LocalDate d, boolean r) {
+		Alarm a = new Alarm(t, d, r);
+		alarms.add(a);
+		//Sort the alarms
+		Collections.sort(alarms);
+	}
+	
+	/** 
+	 * Set alarm with a custom tone
+	 * @param t the time and date the alarm should go off
+	 * @param s placeholder variable for setting a different alarm tone
+	 * @param d the date the alarm is set for
+	 * @param r whether the alarm repeats or not
+	 * 
+	 */
+	public void setAlarm(LocalTime t, LocalDate d, boolean r, int s) {
+		Alarm a = new Alarm(t, d, r);
+		alarms.add(a);
+	}
+	
+	public void unSetAlarm(LocalTime t) {
+		
+	}
+	
+
+	
+	private void tripAlarm(Alarm a){
+		//Code to actually make alarm sound etc goes here
+		//Placeholder to show that an alarm has been tripped 
+		System.out.println("Alarm set for " + a.getTime().toString() + " has been tripped!");
+	}
+	
+	private LocalTime getTime(){
+		//Dummy value since I'm not sure how it works on the RPI
+		return LocalTime.MAX;
+	}
+	
+	private Duration getTrafficShift(){
+		return null;
+	}
+	
+	private Duration getWeatherShift(){
+		return null;
+	}
+	
 	
 	/**
 	 * 
 	 * @author Kurt Floyd
-	 * Uses a comparator to sort based on time. 
+	 * Uses the comparable interface to sort based on time. 
 	 *
 	 */
-	private class Alarm implements Comparator<Alarm> {
+	private class Alarm implements Comparable<Alarm> {
 		private final LocalTime time; //The time the alarm goes off
 		private final boolean repeat; //Whether the alarm repeats
-		//private final int tone; Dummy variable, different tones to be implemented
+		private final LocalDate day;
+		//Private final int tone; Dummy variable, different tones to be implemented
 		
-		Alarm(LocalTime t, boolean r){
+		//Constructor
+		Alarm(LocalTime t, LocalDate d, boolean r){
 			time = t;
 			repeat = r;
+			day = d;
 		}
-		
-		public void setTone(){
-			//Do nothing, to be implemented
-		}
-		
+				
 		public int getTone() {
 			return 0; //Does nothing, to be implemented
 		}
-		
+
 		public boolean doesRepeat() {
 			return repeat;
 		}
 		
 		public LocalTime getTime() {
 			return this.time;
-			
+		}
+		
+		public LocalDate getDay(){
+			return this.day;
 		}
 
 		@Override
-		public int compare(Alarm a1, Alarm a2) {
-			return a1.getTime().compareTo(a2.getTime());
+		public int compareTo(Alarm o) {
+			int dateDif = this.getDay().compareTo(o.getDay());
+			if(dateDif == 0)
+				return this.getTime().compareTo(o.getTime());
+			else 
+				return dateDif;
 		}
 		
-	}
-	
-	//A queue of all alarms.
-	private PriorityQueue<Alarm> alarms;
-	
-	
-	
-	Clock(){
-		alarms = new PriorityQueue<Alarm>();
-	}
-	
-	/**
-	 * Set alarm with the default tone
-	 * @param t
-	 * @param r whether the alarm should repeat or not
-	 */
-	public void setAlarm(LocalTime t, boolean r) {
-		Alarm a = new Alarm(t, r);
-		alarms.add(a);
-	}
-	
-	/** 
-	 * Set alarm with a custom tone
-	 * @param t the time and date the alarm should go off
-	 */
-	public void unSetAlarm(LocalTime t, boolean r) {
-		
-	}
-	
-	public void killAlarm(int i){
-		
-	}
-	
-	private LocalTime getTime(){
-		//Dummy variabe since I'm not sure how it works on the RPI
-		return LocalTime.MAX;
 	}
 
 }
--- src/backend/MainClockProcess.java
+++ src/backend/MainClockProcess.java
@@ -17,20 +17,35 @@ public class MainClockProcess {
 
 	public static void main(String[] args) {
 		Clock c = new Clock();
+		c.setAlarm(LocalTime.now().plusSeconds(30), LocalDate.now(), true);
+		c.setAlarm(LocalTime.now().plusSeconds(3), LocalDate.now(), true);
+
 		//Main loop
+		// Ideally, there'd be a check to see when the next alarm was and sleep until then or until an input interrupt woke it, but this works
 		do {
-			LocalTime today = LocalTime.now();
-			System.out.println(today.toString());
-			
+			LocalTime instant = LocalTime.now();
+			LocalDate today = LocalDate.now();
 			//Get input (running should be set to true or false here)
+			running = true;
 			//Resolve Input
+			checkInput();
 			//Check to see if an alarm would usually be set 
+			c.tick(instant, today);
 			//Check alarms
+			
 		} while (running);
 
 
 	}
 	
+	//Check physical input as well as APIs. Dummy function for now.
+	private static void checkInput(){
+		
+	}
 	
+	//Dummy function, needs to 
+	private void tripAlarm(){
+		
+	}
 
 }
